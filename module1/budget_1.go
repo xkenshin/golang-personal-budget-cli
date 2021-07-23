ce61d45c058f15b5c2e615272dbad7bd6f03a6fc
xkenshin <muhammad.shahril@gmail.com>
2021-07-23 09:56:23 +0800

    PS Adding struct

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,13 @@
 package module1
 
 // Budget stores budget information
+type budget struct {
+	max   float32
+	items []item
+}
 
 // Item stores item information
+type item struct {
+	description string
+	price       float32
+}
